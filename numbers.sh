2ded6b411316460245fff6bb67e9354aa20ef174
jokelley <jokelley@students.chapman.edu>
2025-10-21 13:46:11 -0700

    Add numbers.sh script

---
+++ numbers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#numbers.sh
+# Joshua Kelley
+read -p "Enter a number: " number
+
+for ((i = 1; i <= number; i++)); do
+  if (( i % 2 == 0 )); then
+    echo "$i is even"
+  else
+    echo "$i is odd"
+  fi
+done
